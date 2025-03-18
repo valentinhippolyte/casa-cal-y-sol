67dc7512afe39aadedbe118ca85ec17217bdeccb
valentinhippolyte <74503837+valentinhippolyte@users.noreply.github.com>
2025-03-18 09:14:55 +0100

    fix NavBar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,15 +16,21 @@
     </div>
 
     <ul
-      class="md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-200"
-      :class="isMenuOpen ? 'top-[80px] opacity-100' : 'top-[-400px] opacity-0'"
+      class="md:flex md:items-center absolute md:relative bg-white w-full md:w-auto left-0 md:py-0 py-4 md:pl-0 pl-7 shadow-md md:shadow-none transition-all ease-in duration-200"
+      :class="
+        isMenuOpen
+          ? 'top-[80px] opacity-100 z-50 h-screen w-full'
+          : 'top-[-400px] opacity-0 z-[-1] md:opacity-100 md:static md:z-auto md:flex'
+      "
     >
       <li
         v-for="(link, index) in navLinks"
         :key="index"
         class="mx-4 my-6 md:my-0 text-xl hover:text-cyan-500 duration-200"
       >
-        <RouterLink :to="link.to">{{ link.label }}</RouterLink>
+        <RouterLink :to="link.to" @click="closeMenu">{{
+          link.label
+        }}</RouterLink>
       </li>
     </ul>
   </nav>
@@ -37,6 +43,9 @@ const isMenuOpen = ref(false);
 
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value;
+};
+const closeMenu = () => {
+  isMenuOpen.value = false;
 };
 
 const navLinks = [
