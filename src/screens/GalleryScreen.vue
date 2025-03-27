f272ad57e4ab9e43d4efde80625f2ff83c2dfd57
valentinhippolyte <74503837+valentinhippolyte@users.noreply.github.com>
2025-03-27 11:22:31 +0100

    update style

--- src/screens/GalleryScreen.vue
+++ src/screens/GalleryScreen.vue
@@ -2,11 +2,14 @@
 import MasonryWall from "@yeger/vue-masonry-wall";
 import { ref } from "vue";
 import VueEasyLightbox from "vue-easy-lightbox";
+import { useI18n } from "vue-i18n";
+
+const { t, locale } = useI18n();
 
 const images = ref([
   {
     src: new URL("../assets/images/breakfast.jpg", import.meta.url).href,
-    height: 250,
+    height: 400,
   },
   {
     src: new URL("../assets/images/house_front.jpg", import.meta.url).href,
@@ -14,7 +17,7 @@ const images = ref([
   },
   {
     src: new URL("../assets/images/living_room.jpg", import.meta.url).href,
-    height: 400,
+    height: 250,
   },
   {
     src: new URL("../assets/images/night.jpg", import.meta.url).href,
@@ -56,21 +59,25 @@ const openLightbox = (index) => {
 </script>
 
 <template>
-  <masonry-wall :items="images" :column-width="300" :gap="10">
-    <template #default="{ item, index }">
-      <div
-        :style="{ height: `${item.height}px` }"
-        class="relative cursor-pointer transition-transform duration-200 hover:scale-101"
-        @click="openLightbox(index)"
-      >
-        <img
-          :src="item.src"
-          class="w-full h-full object-cover rounded-lg"
-          alt=""
-        />
-      </div>
-    </template>
-  </masonry-wall>
+  <div class="max-w-[1200px] mx-auto px-4 mt-8 mb-12">
+    <h2 class="text-2xl font-bold mb-4">{{ t("gallery.title") }}</h2>
+    <masonry-wall :items="images" :column-width="250" :gap="10">
+      <template #default="{ item, index }">
+        <div
+          :style="{ height: `${item.height}px` }"
+          class="relative cursor-pointer transition-transform duration-200 hover:scale-101"
+          @click="openLightbox(index)"
+        >
+          <img
+            :src="item.src"
+            class="w-full h-full object-cover rounded-lg"
+            loading="lazy"
+            alt=""
+          />
+        </div>
+      </template>
+    </masonry-wall>
+  </div>
 
   <!-- Lightbox -->
   <vue-easy-lightbox
