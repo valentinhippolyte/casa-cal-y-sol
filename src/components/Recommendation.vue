5a3879a17ee5a2370106790998419568cae97ad1
valentinhippolyte <74503837+valentinhippolyte@users.noreply.github.com>
2025-04-19 13:49:12 +0200

    Update recommandation

--- src/components/Recommendation.vue
+++ src/components/Recommendation.vue
@@ -1,35 +1,22 @@
 <template>
   <div class="grid grid-cols-1 gap-8 items-center lg:grid-cols-2 text-justify">
-    <div class="order-first lg:order-none">
-      <img
-        src="../assets/images/map_olvera.png"
-        alt="Map"
-        class="w-full rounded-lg shadow-lg"
-      />
-    </div>
-    <div class="order-last lg:order-none">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse architecto
-        quod temporibus repellat veniam non similique cupiditate reiciendis, ex
-        ut, eos velit modi ipsa nam dolore vitae vel eius tempora.
-      </p>
-    </div>
+    <img
+      src="../assets/images/map_olvera.png"
+      alt="Map"
+      class="w-full rounded-lg shadow-lg"
+    />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse architecto
+      quod temporibus repellat veniam non similique cupiditate reiciendis, ex
+      ut, eos velit modi ipsa nam dolore vitae vel eius tempora.
+    </p>
   </div>
 
-  <div class="grid grid-cols-1 gap-8 items-center lg:grid-cols-2 text-justify">
-    <div class="order-last lg:order-none">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse architecto
-        quod temporibus repellat veniam non similique cupiditate reiciendis, ex
-        ut, eos velit modi ipsa nam dolore vitae vel eius tempora.
-      </p>
-    </div>
-    <div class="order-first lg:order-none">
-      <img
-        src="../assets/images/map_andalousie.png"
-        alt="Map"
-        class="w-full rounded-lg shadow-lg"
-      />
-    </div>
+  <div class="mx-auto w-1/2 lg:w-1/3">
+    <img
+      src="../assets/images/map_andalousie.png"
+      alt="Map"
+      class="w-auto h-70 rounded-lg shadow-lg"
+    />
   </div>
 </template>
