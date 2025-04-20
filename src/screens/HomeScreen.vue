e6bbd799a621bf0ed6e15b0140ed5d7fba73eb2c
valentinhippolyte <74503837+valentinhippolyte@users.noreply.github.com>
2025-04-20 21:43:33 +0200

    information section from Home page done

--- src/screens/HomeScreen.vue
+++ src/screens/HomeScreen.vue
@@ -1,7 +1,9 @@
 <template>
   <HeroSection />
+  <InformationFromHome />
 </template>
 
 <script setup>
 import HeroSection from "../components/HeroSection.vue";
+import InformationFromHome from "../components/InformationFromHome.vue";
 </script>
