9b745bd7d4d76c52899d0a7db75f2bbac05ed269
valentinhippolyte <74503837+valentinhippolyte@users.noreply.github.com>
2025-04-20 22:56:15 +0200

    characteristic section from home page done

--- src/screens/HomeScreen.vue
+++ src/screens/HomeScreen.vue
@@ -1,9 +1,11 @@
 <template>
   <HeroSection />
   <InformationFromHome />
+  <Characteristic />
 </template>
 
 <script setup>
+import Characteristic from "../components/Characteristic.vue";
 import HeroSection from "../components/HeroSection.vue";
 import InformationFromHome from "../components/InformationFromHome.vue";
 </script>
