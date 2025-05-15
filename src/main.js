e73627b1155728ebd9106598b06dfa9fdd7267fe
Valentin <valenthippo@gmail.com>
2025-05-15 19:26:03 +0200

    optimize import component in routes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import * as lucideIcons from "lucide-vue-next";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { i18n } from "./plugins/i18n";
@@ -6,10 +5,6 @@ import router from "./router";
 import "./style.css";
 
 const app = createApp(App);
-
-for (const [key, component] of Object.entries(lucideIcons)) {
-  app.component(key, component);
-}
 
 app.use(i18n);
 app.use(router);
