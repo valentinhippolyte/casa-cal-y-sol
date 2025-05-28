ddec1eae9eb2d99060dd44f9d3f6b5d7bb1f801f
Valentin <valenthippo@gmail.com>
2025-05-28 13:00:52 +0200

    fix icons import

--- src/components/information/olvera_part/Olvera.vue
+++ src/components/information/olvera_part/Olvera.vue
@@ -24,21 +24,27 @@
 
           <div class="grid grid-cols-2 sm:grid-cols-3 gap-y-4 gap-x-4">
             <LegendItem
-              icon="ShoppingCart"
+              :icon="ShoppingCart"
               :label="t('information.olvera.legend.1')"
             />
             <LegendItem
-              icon="Croissant"
+              :icon="Croissant"
               :label="t('information.olvera.legend.2')"
             />
-            <LegendItem icon="Beer" :label="t('information.olvera.legend.3')" />
             <LegendItem
-              icon="Apple"
+              :icon="Beer"
+              :label="t('information.olvera.legend.3')"
+            />
+            <LegendItem
+              :icon="Apple"
               :label="t('information.olvera.legend.4')"
             />
-            <LegendItem icon="View" :label="t('information.olvera.legend.5')" />
             <LegendItem
-              icon="Footprints"
+              :icon="View"
+              :label="t('information.olvera.legend.5')"
+            />
+            <LegendItem
+              :icon="Footprints"
               :label="t('information.olvera.legend.6')"
             />
           </div>
@@ -68,6 +74,14 @@
 </template>
 
 <script setup>
+import {
+  Apple,
+  Beer,
+  Croissant,
+  Footprints,
+  ShoppingCart,
+  View,
+} from "lucide-vue-next";
 import { ref } from "vue";
 import VueEasyLightbox from "vue-easy-lightbox";
 import { useI18n } from "vue-i18n";
